532264ec13189fb764ade37b19dce3c10741232e
Tim Keresey <tim.keresey@gmail.com>
2020-11-06 21:30:06 -0700

    wrap browser router around App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import App from './App/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const router = <BrowserRouter><App /></BrowserRouter>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render( router, document.getElementById('root'));
+
+serviceWorker.unregister();
