f71c6ed30bc7227cec5fa345d3a481f0a4bdfbb9
Tim Keresey <tim.keresey@gmail.com>
2020-11-06 20:16:47 -0700

    set up App component

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: '',
+      stateUS: '',
+      cityID: '',
+      joints: []
+    }
+  }
+
+  render() {
+    return (
+      <body>
+        <header>
+          <h1> </h1>
+          <
+        </header>
+        <main>
+
+        </main>
+      </body>
+    )
+  }
 }
 
 export default App;
+// prop types
