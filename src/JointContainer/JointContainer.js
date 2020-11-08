7de941e5781fc01126b3d1d4179e450f63232f40
Tim Keresey <tim.keresey@gmail.com>
2020-11-08 12:03:42 -0700

    construct JointContainer element

---
+++ src/JointContainer/JointContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './JointContainer.css';
+
+const JointContainer = () => {
+  return (
+    <section className='container'>
+      <Joint
+      name={}
+      address={}
+      phone={}
+      />
+    </section>
+  )
+}
