9253739bd4b73b9947e82cec2eee61f204d6a465
Tim Keresey <tim.keresey@gmail.com>
2020-11-08 00:46:48 -0700

    build SearchForm element

--- src/SearchForm/SearchForm.js
+++ src/SearchForm/SearchForm.js
@@ -1,1 +1,34 @@
+import React, { Component } from 'react';
+import './SearchForm.css';
 
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      stateUS: ''
+    }
+  }
+
+  render() {
+    return (
+      <form className='search-form'>
+        <label htmlFor='city'>City</label>
+        <input
+        className='search'
+        id='city'
+        value={}
+        onChange={}
+        />
+        <label htmlFor='stateUS'>State</label>
+        <input
+        className='search'
+        id='stateUS'
+        value={}
+        onChange={}
+        />
+        <input type="submit" value="Search"/> 
+      </form>
+    )
+  }
+}
