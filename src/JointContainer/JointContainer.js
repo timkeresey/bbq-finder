ec7c12b50aff5913aeca3932b8c903233d4cbcec
Tim Keresey <tim.keresey@gmail.com>
2020-11-08 18:38:18 -0700

    create conditions for rendering Joints

--- src/JointContainer/JointContainer.js
+++ src/JointContainer/JointContainer.js
@@ -2,15 +2,28 @@ import React from 'react';
 import Joint from '../Joint/Joint';
 import './JointContainer.css';
 
-const JointContainer = () => {
-  return (
-    <section className='container'>
-      <h1>Joints in {}</h1>
-      <Joint
-      name={}
-      address={}
-      phone={}
-      />
-    </section>
-  )
+const JointContainer = ({ city, joints }) => {
+
+  if (joints.length > 0) {
+    return (
+      <h2 className='search-msg'>Which City?</h2>
+    )
+  } else {
+    joints.map(joint => {
+      if (!joint.isFavorite) {
+        return (
+          <section className='container'>
+            <h1>Joints in {}</h1>
+            <Joint
+            name={}
+            address={}
+            phone={}
+            />
+          </section>
+        )
+      }
+    })
+  }
+
 }
+//return conditionally rendered 'pick a city' message
