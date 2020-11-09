df33079347699246eb22adfc12fe737f642a9b5f
Tim Keresey <tim.keresey@gmail.com>
2020-11-09 15:52:13 -0700

    write SearchForm unit test

--- src/SearchForm/SearchForm.test.js
+++ src/SearchForm/SearchForm.test.js
@@ -3,3 +3,18 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import SearchForm from './SearchForm.js';
+
+
+describe('SearchForm', () => {
+  it('should render a search form', () => {
+    const dumSearchCity = jest.fn();
+
+    render (<SearchForm searchCity={dumSearchCity} />);
+
+    expect(screen.getByText('City')).toBeInTheDocument();
+    expect(screen.getByText('State')).toBeInTheDocument();
+    expect(screen.getByText('Search')).toBeInTheDocument();
+  });
+  //should update values on onChange
+  //should fire search method when search button clicked
+})
