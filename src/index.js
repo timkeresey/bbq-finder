149de7477b3ccee22c66fa830b53bcd626055551
Tim Keresey <tim.keresey@gmail.com>
2020-11-09 03:17:03 -0700

    install react router dom

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import App from './App/App';
 
 const router = <BrowserRouter><App /></BrowserRouter>
 
 ReactDOM.render( router, document.getElementById('root'));
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
