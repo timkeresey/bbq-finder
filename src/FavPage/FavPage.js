625323f244269dd4e28a748097f7e9e127fec05d
Tim Keresey <tim.keresey@gmail.com>
2020-11-08 20:02:51 -0700

    add condition for rendering Favorites

--- src/FavPage/FavPage.js
+++ src/FavPage/FavPage.js
@@ -2,15 +2,24 @@ import React from 'react';
 import Favorite from '../Favorite/Favorite';
 import '/.FavPage.css';
 
-const FavPage = () => {
-  return (
-    <section className='page'>
-      <h1>Joints You Gotta Have</h1>
-      <Favorite
-      name={}
-      address={}
-      phone={}
-      />
-    </section>
-  )
+const FavPage = ({ favs }) => {
+  if(favs.length === 0){
+    return (
+      <h2 className='fav-msg'>Pick Some Joints You Gotta Have!</h2>
+    )
+  } else {
+    favs.map(fav => {
+      return (
+        <section className='page'>
+          <h1>Joints You Gotta Have</h1>
+          <Favorite
+          name={}
+          address={}
+          phone={}
+          />
+        </section>
+      )
+    })
+  }
+
 }
