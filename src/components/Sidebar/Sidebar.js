6cffd1bcb030759ba2a3b395f6e4f3ba60e7a43a
Tim Keresey <tim.keresey@gmail.com>
2021-05-19 16:44:56 -0600

    add sidebar component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Sidebar.scss';
+
+const Sidebar = () => {
+
+    return (
+        <div>
+            SIDEBAR
+        </div>
+    )
+}
+
+export default Sidebar;
