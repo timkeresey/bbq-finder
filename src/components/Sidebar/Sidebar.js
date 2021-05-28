fb976be5c2ef9c424b8c52fbda89627019c6c7e9
Tim Keresey <tim.keresey@gmail.com>
2021-05-27 18:51:43 -0600

    style sidebar animation

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,11 +5,14 @@ const Sidebar = () => {
 
     return (
         <div className='sidebar'>
-            <h3 className='sidebar__heading'>Your Favs</h3>
             <input type='checkbox' className='sidebar__checkbox' id='side-toggle'/>
             <label for='side-toggle' className='sidebar__btn'>
                 <span className='sidebar__icon'>&nbsp;</span>
             </label>
+            <h3 className='sidebar__heading'>Your Favs</h3>
+            <div className='sidebar__background'>
+                &nbsp;
+            </div>
 
         </div>
     )
