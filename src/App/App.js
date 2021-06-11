574e03010326045738e0b8aa6d9f2ad14d1c8279
Tim Keresey <tim.keresey@gmail.com>
2021-06-11 11:13:03 -0600

    refactor video layout

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,9 @@
 import React, { useState, createContext } from 'react';
+import VideoLooper from 'react-video-looper';
 import JointContainer from '../components/JointContainer/JointContainer';
 import Header from '../components/Header/Header';
 import Sidebar from '../components/Sidebar/Sidebar';
+import Roast from '../images/roast.mp4';
 import './App.scss';
 import '../components/JointContainer/JointContainer.scss'
 import { getCityID, getJoints } from '../apiCalls.js';
@@ -29,16 +31,23 @@ const App = () => {
     return (
       <div className='container'>
         <FavoritesContext.Provider value={{ favorites, setFavorites }}>
-          <Header
-            searchCity={searchCity}
-          />
 
-          <Sidebar favorites={favorites}/>
+          <Header searchCity={searchCity} />
+
+          <Sidebar favorites={favorites} />
           
+          {joints.length === 0 ?
+          <div className='video'>
+            <VideoLooper className='video__embed' source={Roast} start={0} end={4} />
+            <h2 className='video__text'>Search for some BBQ!</h2>
+          </div> :
           <section className='joints'>
             <JointContainer joints={joints} />
           </section>
-        </FavoritesContext.Provider>  
+          }
+
+        </FavoritesContext.Provider> 
+
       </div>
     )
   
--- src/components/JointContainer/JointContainer.js
+++ src/components/JointContainer/JointContainer.js
@@ -1,35 +1,23 @@
 import React from 'react';
-import VideoLooper from 'react-video-looper';
 import PropTypes from 'prop-types';
 import Joint from '../Joint/Joint';
 import './JointContainer.scss';
-import Roast from '../../images/roast.mp4';
 
 const JointContainer = ({ joints }) => {
 
-  if (joints.length === 0) {
+  return joints.map(joint => {
     return (
-      <div className='video'>
-        <VideoLooper className='video__embed' source={Roast} start={0} end={4} />
-        <h2 className='video__text'>Search for some BBQ!</h2>
-      </div>
+      <Joint 
+        favorited={false}
+        id={joint.restaurant.id}
+        key={joint.restaurant.id}
+        name={joint.restaurant.name}
+        address={joint.restaurant.location.address}
+        phone={joint.restaurant.phone_numbers}
+        image={joint.restaurant.featured_image}
+      />
     )
-    } else {
-      return joints.map(joint => {
-        return (
-          <Joint 
-            favorited={false}
-            id={joint.restaurant.id}
-            key={joint.restaurant.id}
-            name={joint.restaurant.name}
-            address={joint.restaurant.location.address}
-            phone={joint.restaurant.phone_numbers}
-            image={joint.restaurant.featured_image}
-          />
-        )
-      })
-    }
-
+  })
 }
 
 export default JointContainer;
